19fe75a9cc5c29d4e4782b74f19ad8398393d075
Mpamugo Emmanuel <emmanuelmpamugo2002@gmail.com>
2024-12-28 15:53:16 +0100

    beginning of the header section

---
+++ app/welcome/Components/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <>
+        <section>
+            <div>
+                 <img src="" alt="logo" />
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Service</a>
+                <a href="">Portfolio</a>
+                <a href="">Blog</a>
+                <a href="">Contact</a>  
+            </div>
+        </section>
+        <section>
+            <div>
+            
+
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+           
+        </>
+        
+    )
+}
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,9 +1,12 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import Button from '@mui/material/Button';
+import Header from "./Components/Header";
 
 export function Welcome() {
   return (
-    <p>hello people</p>
+    <Header />
+    
   );
 }
 
